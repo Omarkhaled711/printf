fc33e994739489a4dbe5065b92e220b38ccefff5
Omar Khaled <o.khaled6789@gmail.com>
2023-04-24 23:24:07 +0200

    edited unsigned_to_binary.c

--- unsigned_to_binary.c
+++ unsigned_to_binary.c
@@ -6,30 +6,31 @@
  * Return: int
  *
  */
+
 int unsigned_to_binary(unsigned int num)
 {
+  char result[sizeof(num) * 8];
+  int count = 0, len = 0;
 
-	char result[sizeof(num) * 8];
-	int count = 0, len = 0;
+  if (num == 0)
+  {
+    _putchar('0');
+    return 1;
+  }
 
-	while (num)
-	{
-		result[count++] = (((num & 1) == 1) ? '1' : '0');
-		num >>= 1;
-	}
+  while (num)
+  {
+    result[count++] = (((num & 1) == 1) ? '1' : '0');
+    num >>= 1;
+  }
 
-	if (count)
-	{
-		count--;
-		while (count >= 0)
-		{
-			_putchar(result[count--]);
-			len++;
-		}
-	}
-	else
-	{
-		_putchar('0');
-	}
-	return (len);
+  count--;
+  while (count >= 0)
+  {
+    _putchar(result[count--]);
+    len++;
+  }
+
+  return (len);
 }
+
