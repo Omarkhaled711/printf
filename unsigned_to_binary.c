caedd70e4b96854a6fc2853b29796758d0e505af
Omar Khaled <o.khaled6789@gmail.com>
2023-04-25 05:12:41 +0200

    edited unsigned_to_binary according to betty style

--- unsigned_to_binary.c
+++ unsigned_to_binary.c
@@ -9,28 +9,27 @@
 
 int unsigned_to_binary(unsigned int num)
 {
-  char result[sizeof(num) * 8];
-  int count = 0, len = 0;
+	char result[sizeof(num) * 8];
+	int count = 0, len = 0;
 
-  if (num == 0)
-  {
-    _putchar('0');
-    return 1;
-  }
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
 
-  while (num)
-  {
-    result[count++] = (((num & 1) == 1) ? '1' : '0');
-    num >>= 1;
-  }
+	while (num)
+	{
+		result[count++] = (((num & 1) == 1) ? '1' : '0');
+		num >>= 1;
+	}
 
-  count--;
-  while (count >= 0)
-  {
-    _putchar(result[count--]);
-    len++;
-  }
-
-  return (len);
+	count--;
+	while (count >= 0)
+	{
+		_putchar(result[count--]);
+		len++;
+	}
+	return (len);
 }
 
