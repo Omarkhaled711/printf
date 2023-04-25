46ac7fe72df60c37432be8edbac1133e5d761165
Omar Khaled <o.khaled6789@gmail.com>
2023-04-26 01:28:19 +0200

    added a missing file

--- print_unsigned.c
+++ print_unsigned.c
@@ -4,15 +4,25 @@
  * print_unsignedNum - print unsigned number
  *
  * @num: the unsigned number
+ * @flag: check flags
  * Return: len
  */
-int print_unsignedNum(unsigned int num)
+int print_unsignedNum(unsigned long num, int flag)
 {
 	int len = 0;
 
+	if ((flag >> 6) & 1)
+	{
+		num = (unsigned short) num;
+	}
+	else if (!((flag >> 5) & 1))
+	{
+		num = (unsigned int) num;
+	}
+	flag = 0;
 	if (num >= 10)
 	{
-		len += print_unsignedNum(num / 10);
+		len += print_unsignedNum(num / 10, flag);
 	}
 	_putchar('0' + (num % 10));
 	len++;
