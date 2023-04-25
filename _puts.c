ecdc00a2c7053bda8ca50844b81e8618ce121f40
Omar Khaled <o.khaled6789@gmail.com>
2023-04-25 05:08:53 +0200

    edited _puts.c

--- _puts.c
+++ _puts.c
@@ -15,3 +15,17 @@ int _puts(char *str)
 	}
 	return (i);
 }
+/**
+ * print_string - prints a string
+ *
+ * @formatPtr: pointer to the argument list
+ * @len: pointer to the length of what was printed
+ */
+void print_string(va_list formatPtr, int *len)
+{
+	char *str = va_arg(formatPtr, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	*len += _puts(str) - 1;
+}
